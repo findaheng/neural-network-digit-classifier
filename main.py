ca070675e30546373a01ab76009406a019366be7
Francis Indaheng <findaheng@berkeley.edu>
2019-09-21 18:31:30 -0700

    defined loss function and optimization algorithm

--- main.py
+++ main.py
@@ -33,6 +33,7 @@ dropout = 0.5  # to help overfitting
 X = tf.placeholder("float", [None, num_input]) 
 Y = tf.placeholder("float", [None, num_output])
 keep_prob = tf.placeholder(tf.float32)  # to control the dropout rate
+
 weights = {  # random values close to zero
 	'w1': tf.Variable(tf.truncated_normal([num_input, num_hidden1], stddev=0.1)),
 	'w2': tf.Variable(tf.truncated_normal([num_input, num_hidden2], stddev=0.1)),
@@ -44,6 +45,18 @@ biases = {  # small constant value to ensure tensors activate in initial stages
 	'b2': tf.Variable(tf.constant(0.1, shape=[num_hidden2])),
 	'b3': tf.Variable(tf.constant(0.1, shape=[num_hidden3])),
 	'out': tf.Variable(tf.constant(0.1, shape=[num_output])),
-
 }
+
+layer_1 = tf.add(tf.matmul(X, weights['w1']), biases['b1'])
+layer_2 = tf.add(tf.matmul(layer_1, weights['w2']), biases['b2'])
+layer_3 = tf.add(tf.matmul(layer_2, weights['w3']), biases['b3'])
+layer_drop = tf.nn.dropout(layer3, keep_prob)
+output_layer = tf.matmul(layer_3, weights['out']) + biases['out']
+
+cross_entropy = tf.reduce_mean(
+	tf.nn.softmax_cross_entropy_with_logits(
+		labels=Y, logits=output_layer))  # loss function
+
+train_step = tf.train.AdamOptimizer(learning_rate).minimize(cross_entropy)
+
 
