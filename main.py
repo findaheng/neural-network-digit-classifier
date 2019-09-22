e1c45ee1a46f9ea6c5837b2f54f524335001e94a
Francis Indaheng <findaheng@berkeley.edu>
2019-09-21 17:31:06 -0700

    defined number of layers and size of each layer

--- main.py
+++ main.py
@@ -2,3 +2,14 @@ import tensorflow as tensorflow
 from tensorflow.examples.tutorials.mnist import input_data
 
 mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+
+num_training = mnist.train.num_examples  # 55,000
+num_validation = mnist.validation.num_examples  #5,000
+num_testing = mnist.test.num_examples  #10,000
+
+num_input = 784  # since 28x28 pixels
+num_hidden1 = 512
+num_hidden2 = 256
+num_hidden3 = 128
+num_output = 10  #since 0-9 digits
+
